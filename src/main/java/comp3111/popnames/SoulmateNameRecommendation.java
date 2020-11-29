95ce9e41a700659fc3550718019129db48acd4c5
TAMTszChung <tctam@connect.ust.hk>
2020-11-29 21:20:37 +0800

    Application 2: implement prediction with NK-T5 algorithm

--- src/main/java/comp3111/popnames/SoulmateNameRecommendation.java
+++ src/main/java/comp3111/popnames/SoulmateNameRecommendation.java
@@ -18,6 +18,26 @@ public class SoulmateNameRecommendation {
 		}
 		
 		public String getSoulmateName() {
-			return "";
+			//boolean noReasonableRecommand = false;
+			int oRank = AnalyzeNames.getRankEnhanced(userYOB, userName, userGender);
+			if (oRank == -1) {
+				//noReasonableRecommand = true;
+				oRank = 1;
+			}
+
+			int oYOB;
+			if (soulmateAgePreference.equals("younger")) {
+				oYOB = userYOB - 1;
+			}else {
+				oYOB = userYOB + 1;
+			}
+
+			String oName = AnalyzeNames.getNameEnhanced(oYOB, oRank, soulmateGender);
+			if (oName.contentEquals("information on the name at the specified rank is not available")) {
+				//noReasonableRecommand = true;
+				oName = AnalyzeNames.getNameEnhanced(oYOB, 1, soulmateGender);
+			}
+
+			return oName;
 		}
 }
