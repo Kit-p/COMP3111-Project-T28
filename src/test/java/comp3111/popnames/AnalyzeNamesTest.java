ac11415ab7001d48e61c5ddb2cb192ed1768fe01
PANG, Kit <kpangaa@connect.ust.hk>
2020-12-03 01:43:51 +0800

    Unit-test helper functions

--- src/test/java/comp3111/popnames/AnalyzeNamesTest.java
+++ src/test/java/comp3111/popnames/AnalyzeNamesTest.java
@@ -1,7 +1,9 @@
 package comp3111.popnames;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AnalyzeNamesTest {
 	
@@ -38,6 +40,41 @@ public class AnalyzeNamesTest {
     	AnalyzeNames a = new AnalyzeNames();
     	String name = a.getName(2019, 2192, "F");
     	assertTrue(name.equals("Desire"));
+    }
+
+    @Test
+    public void testGetRankEnhanced() {
+        int rankA = AnalyzeNames.getRankEnhanced(2009, "Nathan", "M");
+        int rankB = AnalyzeNames.getRankEnhanced(2009, "Jackson", "M");
+        assertEquals(rankA, rankB);
+        int rank = AnalyzeNames.getRankEnhanced(2009, "", "");
+        assertEquals(-1, rank);
+    }
+
+    @Test
+    public void testGetTopNNamesInRangeYears() {
+        String[] result = AnalyzeNames.getTopNNamesInRangeYears("M", 1881, 1882, 1935);
+        assertTrue(result.length > 0);
+        result = AnalyzeNames.getTopNNamesInRangeYears("F", 1881, 1882, 0);
+        assertEquals(0, result.length);
+    }
+
+    @Test
+    public void testGetLowestRankInRangeYears() {
+        Integer[] result = AnalyzeNames.getLowestRankInRangeYears("Emilia", "F", 1897, 1899);
+        assertEquals(2, result.length);
+    }
+
+    @Test
+    public void testGetHighestRankInRangeYears() {
+        Integer[] result = AnalyzeNames.getHighestRankInRangeYears("Emilia", "F", 1897, 1899);
+        assertEquals(2, result.length);
+    }
+
+    @Test
+    public void testGetGenderLowestRankOfYear() {
+        int lowestRank = AnalyzeNames.getGenderLowestRankOfYear("M", 1881);
+        assertTrue(lowestRank > 1);
     }
 
 }
