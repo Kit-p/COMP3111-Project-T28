63a03146fc32b92a4711e1404ef4055fc3b3b421
PANG, Kit <kpangaa@connect.ust.hk>
2020-12-03 23:22:56 +0800

    Update Application 3 algorithm justification

--- src/main/java/comp3111/popnames/CompatibilityPrediction.java
+++ src/main/java/comp3111/popnames/CompatibilityPrediction.java
@@ -55,22 +55,27 @@ public class CompatibilityPrediction {
     /**
      * Get the compatibility prediction of the user and the mate
      *
+     * <h3>Algorithm:</h3>
      * <p>
-     * <strong>Algorithm:</strong><br>
      * The algorithm computes the difference between the ranks of the pair in their own year,
      * and the maximum possible difference between the ranks of the pair in their own year.
      * The ratio implies how close their ranks are and is then converted to percentage (corrected to 2 d.p.).
      * The percentage is the predicted compatibility of the pair.
      * </p>
      *
+     * <h3>Justification:</h3>
      * <p>
-     * <strong>Justification:</strong><br>
      * The original NK-T6 Algorithm of Universal Compatibility computes a variation of the percentage difference,
      * which will result in value that are smaller than 0% or greater than 100%.
      * The modified algorithm uses the rank difference as the numerator and the maximum difference as the denominator.
      * Since the difference is always greater than or equal to zero,
      * and is always smaller than or equal to the maximum difference,
      * the result will always be between 0% and 100%, which is consistent to the expected output format.
+     * </p>
+     * <p>
+     * The algorithm is used because we believe it is difficult for the two names of the pair to have same rank.
+     * The closer the ranks are, the lower the possibility is.
+     * Such rarity resembles the concept of serendipity, which we use as the criteria for predicting the compatibility.
      * </p>
      *
      * @return the compatibility scaled by 100 (for percentage)
