c30b8ebb908fa71b2615eae925b2e84b0075865b
TAMTszChung <tctam@connect.ust.hk>
2020-12-04 20:33:16 +0800

    Application 2: fix typos in JavaDoc

--- src/main/java/comp3111/popnames/SoulmateNameRecommendation.java
+++ src/main/java/comp3111/popnames/SoulmateNameRecommendation.java
@@ -53,21 +53,21 @@ public class SoulmateNameRecommendation {
 	 * <p>
 	 * <strong>Algorithm:</strong>
 	 * <br>
-	 * The algorithm predict the name of the soulmate of the user by finding the name that have the closest rank to the user's name rank
+	 * The algorithm predicts the name of the soulmate of the user by finding the name that has the closest rank to the user's name rank
 	 * with the preferred gender in the calculated soulmate's year of birth.
 	 * <br>
-	 * The algorithm first calculate the rank of the user's name in his/her year of birth.
+	 * The algorithm first calculates the rank of the user's name in his/her year of birth.
 	 * <br>
 	 * Then, calculate the year of birth of the soulmate, if the preference is "younger", the soulmate's year of birth is (user's year of birth - 1),
-	 * else if the perference is "older", the soulmate's year of birth is (user's year of birth + 1).
+	 * else if the preference is "older", the soulmate's year of birth is (user's year of birth + 1).
 	 * <br>
-	 * Finally, the algorithm finds the name in the soulmate's year of birth that have the same rank as the user's name rank,
+	 * Finally, the algorithm finds the name in the soulmate's year of birth that has the same rank as the user's name rank,
 	 * which the closest possible rank to the user's name rank, and returns that name.
 	 * <br>
 	 * If there do not exist that has the same rank as the user's name rank, either due to the user's name is not ranked, or
 	 * the user's name rank is too low that there is not a name in the soulmate's year of birth with that rank,
-	 * the algorithm returns lowest ranked name in the soulmate's year of birth,
-	 * which has the closest possible rank with names that is not ranked.
+	 * the algorithm returns lowest-ranked name in the soulmate's year of birth,
+	 * which has the closest possible rank with name that is not ranked.
 	 * </p>
 	 *
 	 * <p>
@@ -77,7 +77,7 @@ public class SoulmateNameRecommendation {
 	 * with the closest rank to the user's name rank in the user's year of birth.
 	 * <br>
 	 * This is because the rank of the name basically represents the popularity of a name in the year (as higher rank name has larger occurrence).
-	 * With so many possible names, it is very difficult the name of 2 people to be of the same rank and be equally popular in their year of birth.
+	 * With so many possible names, it is very difficult for the name of 2 people to be of the same rank and be equally popular in their year of birth.
 	 * <br>
 	 * The closer the ranks of the 2 names are, the lower the possibility is.
 	 * Such rarity resembles the concept of serendipity, which we use as the criteria for predicting the name of the soulmate of the user.
@@ -86,22 +86,22 @@ public class SoulmateNameRecommendation {
 	 * people in the same generation should have more common knowledge and interest, so age difference of 1 should make 2 people a good pair.
 	 * <br>
 	 * <br>
-	 * The original NK-T5 Algorithm of Universal Compatibility also use the above principle to predicts soulmate's name.
+	 * The original NK-T5 Algorithm of Universal Compatibility also uses the above principle to predicts soulmate's name.
 	 * <br>
-	 * However, if the user's name is not ranked, or user's name rank is too low that there is not a name in the soulmate's year of birth with that rank,
-	 * the original algorithm use the top ranked name in the soulmate's year of birth as the predicted name.
+	 * However, if the user's name is not ranked, or the user's name rank is too low that there is not a name in the soulmate's year of birth with that rank,
+	 * the original algorithm uses the top-ranked name in the soulmate's year of birth as the predicted name.
 	 * Thus, the predicted name will not have the closest rank to the user's name,
 	 * but instead having the largest difference in rank and popularity.
-	 * This do not resembles the concept of serendipity, and the name prediction is not coherent in all cases.
+	 * This does not resemble the concept of serendipity, and the name prediction is not coherent in all cases.
 	 * <br>
 	 * <br>
-	 * Therefore, the modified algorithm return the lowest ranked name in the soulmate's year of birth in such case.
-	 * As name that is not ranked is name below the lowest ranked name, the lowest ranked name will have the closest rank
+	 * Therefore, the modified algorithm returns the lowest-ranked name in the soulmate's year of birth in such case.
+	 * As a name that is not ranked is below the lowest-ranked name, the lowest-ranked name will have the closest rank
 	 * to the not ranked name.
 	 * Thus, the modified algorithm will always recommend the name with the closest rank to the user's name rank,
-	 * making coherent recommendation in all cases.
+	 * making coherent recommendations in all cases.
 	 * <br>
-	 * Therefore, modified algorithm gives justifiable and coherent prediction of soulmate name in all cases.
+	 * Therefore, the modified algorithm gives justifiable and coherent predictions for the soulmate name in all cases.
 	 * </p>
 	 *
 	 * @return the predicted name for the soulmate
